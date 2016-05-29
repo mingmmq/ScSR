b922948a2fead4e2d5dea3c6d9e4ffc1b587b391
MinmingQian <a1675754@student.adelaide.edu.au>
2016-05-29 15:18:25 +0930

    all files

---
+++ Training.m
@@ -0,0 +1,16 @@
+
+%clear all; clc; close all;
+%Demo_Dictionary_Training('original', 'Data/Training/original', 1024);
+
+Demo_Dictionary_Training('original', 'Data/Training/Original', 128);
+
+Demo_Dictionary_Training('original', 'Data/Training/Original', 256);
+
+%clear all; clc; close all;
+Demo_Dictionary_Training('original', 'Data/Training/Original', 512);
+
+%clear all; clc; close all;
+
+%clear all; clc; close all;
+Demo_Dictionary_Training('original', 'Data/Training/Original', 2048);
+
---
+++ TrainingSize.m
@@ -0,0 +1,80 @@
+
+clear all; clc; close all;
+Demo_Dictionary_Training('building', 'Data/Training/Building', 1024);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('butterfly', 'Data/Training/Butterfly', 1024);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('face', 'Data/Training/Face', 1024);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('flower', 'Data/Training/Flower', 1024);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('food', 'Data/Training/Food', 1024);
+
+
+
+
+
+clear all; clc; close all;
+Demo_Dictionary_Training('building', 'Data/Training/Building', 512);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('butterfly', 'Data/Training/Butterfly', 512);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('face', 'Data/Training/Face', 512);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('flower', 'Data/Training/Flower', 512);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('food', 'Data/Training/Food', 512);
+
+
+
+
+
+clear all; clc; close all;
+Demo_Dictionary_Training('building', 'Data/Training/Building', 256);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('butterfly', 'Data/Training/Butterfly', 256);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('face', 'Data/Training/Face', 256);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('flower', 'Data/Training/Flower', 256);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('food', 'Data/Training/Food', 256);
+
+
+
+
+
+
+
+
+
+clear all; clc; close all;
+Demo_Dictionary_Training('building', 'Data/Training/Building', 2048);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('butterfly', 'Data/Training/Butterfly', 2048);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('face', 'Data/Training/Face', 2048);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('flower', 'Data/Training/Flower', 2048);
+
+clear all; clc; close all;
+Demo_Dictionary_Training('food', 'Data/Training/Food', 2048);
+
+
+
+
