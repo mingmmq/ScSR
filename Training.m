d69a854d230b685e3a41b01afea04ef187ebdb3c
MinmingQian <a1675754@student.adelaide.edu.au>
2016-05-29 16:59:58 +0930

    update

--- Training.m
+++ Training.m
@@ -9,7 +9,7 @@ Demo_Dictionary_Training('original', 'Data/Training/Original', 256);
 %clear all; clc; close all;
 Demo_Dictionary_Training('original', 'Data/Training/Original', 512);
 
-%clear all; clc; close all;
+%clear all; clc; close all; 
 
 %clear all; clc; close all;
 Demo_Dictionary_Training('original', 'Data/Training/Original', 2048);
